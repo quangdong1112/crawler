848cc01f5ad5b6c2db03023a43a8169593603948
Pham Van Duc <vn_ducpv2@vnext.vn>
2022-06-27 23:24:39 +0700

    find-data

--- app/Console/Commands/Crawler/CrawlerDataCommand.php
+++ app/Console/Commands/Crawler/CrawlerDataCommand.php
@@ -4,7 +4,6 @@ namespace App\Console\Commands\Crawler;
 
 use Illuminate\Console\Command;
 use Weidner\Goutte\GoutteFacade;
-ini_set('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36');
 class CrawlerDataCommand extends Command
 {
     protected $signature = 'crawler:init';
@@ -32,12 +31,19 @@ class CrawlerDataCommand extends Command
     {
         $linkProduct = "https://nha.chotot.com/thue-bat-dong-san";
         $html = file_get_html($linkProduct);
-        foreach ($html->find('#__next > div > div.container.ct-listing > div.styles_listViewWrapper__162PR > div.styles_base__2D2wo > main > div.no-padding.col-md-8.ct-col-8 > div.list-view > div > div:nth-child(1) > ul:nth-child(1) > div:nth-child(1) > li > a') as $link) {
-            $href = $link->getAttribute('href') ?? '';
-            $text = $link->childNodes(1)->plaintext;
+        $priceBox = [];
+        foreach ($html->find('div.list-view li') as $link) {
+            $href = $link->find('a',0)->href ?? '';
+            $text = $link->find('.webpimg-container img' ,0)->alt ?? '';
+            $title = $link->find('.Layout_right__2f2Ss h3' ,0)->plaintext ?? '';
+            $area = $link->find('.Layout_right__2f2Ss span' ,0)->plaintext ?? '';
+            $price = $link->find('.Layout_right__2f2Ss p' ,0)->plaintext ?? '';
             $priceBox[] = [
                 'name' => $text,
                 'link' => $href,
+                'title' => $title,
+                'area' => $area,
+                'price' => $price,
             ];
         }
         dd($priceBox);
